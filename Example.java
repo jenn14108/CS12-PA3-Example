72b60dbd4c642579e5a5ac38f9e2fd6f4aebe230
jenn14108 <jenn.lee14108@gmail.com>
2019-01-07 10:44:09 -0500

    Add checkSusSubmission method

--- Example.java
+++ Example.java
@@ -1,5 +1,6 @@
 public class Example {
 	public static void main(String[] args){
 		System.out.println("Hello this is an example");
+		System.out.println("Making a random change so I can commit");
 	}
 }
